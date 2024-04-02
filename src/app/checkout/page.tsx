ac232a24db3e6ed963abc40983ffa6854713f407
Chris <chrisdietrich3666@gmail.com>
2024-04-02 18:01:56 -0400

    styled delivery details page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -25,8 +25,8 @@ const Checkout = () => {
 
   const router = useRouter();
 
-  router.prefetch('/login')
-  router.prefetch('/delivery-details')
+  router.prefetch("/login");
+  router.prefetch("/delivery-details");
 
   const handleRedirect = () => {
     if (!userId) {
@@ -39,51 +39,89 @@ const Checkout = () => {
   // useEffect(() => {
   //   fetch("http://localhost:3000/api/cart", {
   //     headers: {
-  //       "x-decoded-token": 
+  //       "x-decoded-token":
   //     }
   //   })
   // }, [])
 
   return (
     <Container className="main-content">
-      <Grid container spacing={2}>
-        <Grid item lg={9}>
-          {cart.map((cartItem) => {
-            return (
-              <>
-                <Card>
-                  <CardContent sx={{ display: "flex", columnGap: 3 }}>
-                    <Image
-                      width={333}
-                      height={333}
-                      alt="Picture of Julie"
-                      src="/images/art1.jpg"
-                    />
-                    <Stack className={styles.card_content_items}>
-                      <Typography variant="h3">{cartItem.title}</Typography>
-                      <Typography variant="h5">${cartItem.price}</Typography>
-                      <Typography style={{ marginTop: "20px" }}>
-                        Measurements: {cartItem.measurements}
-                      </Typography>
-                    </Stack>
-                  </CardContent>
-                  <CardActions></CardActions>
-                </Card>
-              </>
-            );
-          })}
-        </Grid>
+      <table className="table">
+        <tr>
+          <th>Image</th>
+          <th className="text-left">Name</th>
+          <th>Measurements</th>
+          <th className="text-right">Price</th>
+        </tr>
 
-        <Grid item lg={3}>
-          {" "}
-          <Card>
-            <Button onClick={handleRedirect}>
-              Proceed to delivery details
-            </Button>
-          </Card>
-        </Grid>
-      </Grid>
+        {cart.map((cartItem) => {
+          return (
+            <>
+              <tr>
+                <td>
+                  <Image
+                    width={100}
+                    height={100}
+                    alt="Picture of Julie"
+                    src="/images/art1.jpg"
+                  />
+                </td>
+                <td>{cartItem.title}</td>
+                <td className="text-center"> {cartItem.measurements}</td>
+                <td className="text-right">${cartItem.price}</td>
+              </tr>
+            </>
+          );
+        })}
+      </table>
+      <Button
+        style={{ margin: "20px 0 0 auto" }}
+        className="btn btn-large"
+        onClick={handleRedirect}
+      >
+        Proceed to delivery details
+      </Button>
     </Container>
+    // <Container className="main-content">
+    //   <Grid container spacing={2}>
+    //     <Grid item lg={9}>
+    //       {cart.map((cartItem) => {
+    //         return (
+    //           <>
+    //             <Card elevation={0}>
+    //               <CardContent sx={{ display: "flex", columnGap: 3 }}>
+    //                 <Image
+    //                   width={333}
+    //                   height={333}
+    //                   alt="Picture of Julie"
+    //                   src="/images/art1.jpg"
+    //                 />
+    //                 <Stack className={styles.card_content_items}>
+    //                   <Typography variant="h3">{cartItem.title}</Typography>
+    //                   <Typography variant="h5">${cartItem.price}</Typography>
+    //                   <Typography style={{ marginTop: "20px" }}>
+    //                     Measurements: {cartItem.measurements}
+    //                   </Typography>
+    //                   <Button
+    //                     style={{ marginTop: "20px" }}
+    //                     className="btn btn-large"
+    //                     onClick={handleRedirect}
+    //                   >
+    //                     Proceed to delivery details
+    //                   </Button>
+    //                 </Stack>
+    //               </CardContent>
+    //             </Card>
+    //           </>
+    //         );
+    //       })}
+    //     </Grid>
+
+    //     <Grid item lg={3}>
+    //       {" "}
+    //     </Grid>
+    //   </Grid>
+    // </Container>
   );
 };
 
--- src/app/delivery-details/page.tsx
+++ src/app/delivery-details/page.tsx
@@ -1,33 +1,41 @@
 "use client";
-import React, {useContext, useEffect, useState} from "react";
-import {Elements} from '@stripe/react-stripe-js';
-import {Box, Button, CircularProgress, Container, Divider, Stack, Typography} from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Divider,
+  Stack,
+  Typography,
+} from "@mui/material";
 import CheckoutForm from "@/components/forms/CheckoutForm/CheckoutForm";
-import {loadStripe, StripeElementsOptions} from "@stripe/stripe-js";
-import {cartContext} from "@/app/context/cartContext";
-import {StripeApi} from "@/services";
-import {currencyFormat} from "@/helpers";
-import {LoadingButton} from "@mui/lab";
+import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
+import { cartContext } from "@/app/context/cartContext";
+import { StripeApi } from "@/services";
+import { currencyFormat } from "@/helpers";
+import { LoadingButton } from "@mui/lab";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY as string);
 
 const CreateAccount: React.FC = () => {
-  const {cart} = useContext(cartContext)
-  const [clientSecret, setClientSecret] = useState('');
-  const [amountFormatted, setAmountFormatted] = useState('');
+  const { cart } = useContext(cartContext);
+  const [clientSecret, setClientSecret] = useState("");
+  const [amountFormatted, setAmountFormatted] = useState("");
   const [disabled, setDisabled] = useState(true);
   const [loading, setLoading] = useState(false);
 
   const createPaymentIntent = async () => {
-    const response = await StripeApi.CreatePaymentIntent(cart)
+    const response = await StripeApi.CreatePaymentIntent(cart);
 
-    const {data} = response;
-    console.log(cart)
+    const { data } = response;
+    console.log(cart);
 
-    setAmountFormatted(currencyFormat(data.amount))
+    setAmountFormatted(currencyFormat(data.amount));
 
     setClientSecret(data.clientSecret);
-  }
+  };
 
   useEffect(() => {
     if (cart.length) {
@@ -38,88 +46,104 @@ const CreateAccount: React.FC = () => {
   const options: StripeElementsOptions = {
     clientSecret,
     appearance: {
-      theme: 'stripe'
+      theme: "stripe",
     },
     loader: "auto",
   };
-  return (
-    !clientSecret ?
-      <Box
-        textAlign="center"
-        paddingY={2}
-        height="64svh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
+  return !clientSecret ? (
+    <Box
+      textAlign="center"
+      paddingY={2}
+      height="64svh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <CircularProgress />
+    </Box>
+  ) : (
+    <Elements stripe={stripePromise} options={options}>
+      <Container
+        maxWidth="xl"
+        sx={{
+          paddingY: 5,
+        }}
       >
-        <CircularProgress/>
-      </Box>
-      :
-      <Elements stripe={stripePromise} options={options}>
-        <Container maxWidth="xl" sx={{
-          paddingY: 5
-        }}>
-          <Stack direction="row" columnGap={5}>
-            <Box
-              flex={1}
-              padding={0}
-              margin={0}
-              minHeight="56svh"
-              bgcolor="#fff"
-              textAlign="center"
+        <Stack direction="row" columnGap={5}>
+          <Box
+            flex={1}
+            padding={0}
+            margin={0}
+            minHeight="56svh"
+            bgcolor="#fff"
+            textAlign="center"
+          >
+            <Typography
+              sx={{ textAlign: "center", my: 3 }}
+              variant="h1"
+              component="h1"
             >
-              <Typography
-                component="p"
-                color="black"
-                sx={{
-                  paddingY: 3
-                }}
-              >Fill the information below</Typography>
-              <CheckoutForm clientSecret={clientSecret} onDisabled={(value) => setDisabled(value)} onLoad={(load) => setLoading(load)} />
-            </Box>
-            <Stack flex={1} sx={{
-              border: '1px solid rgba(0,0,0, 0.2)',
+              Payment
+            </Typography>
+            <CheckoutForm
+              clientSecret={clientSecret}
+              onDisabled={(value) => setDisabled(value)}
+              onLoad={(load) => setLoading(load)}
+            />
+          </Box>
+          <Stack
+            flex={1}
+            sx={{
+              border: "1px solid rgba(0,0,0, 0.2)",
               borderRadius: 1,
               padding: 2,
               columnGap: 2,
-              justifyContent: 'space-between'
-            }}>
-              <Box>
-                <Typography variant="h3" fontSize={18} sx={{
-                  marginBottom: 2
-                }}>Products</Typography>
-                <Stack rowGap={1} divider={<Divider />}>
-
-                {
-                  cart.map(({title, price}, index) => (
-                <Stack key={`product-item-${index}`}>
-                  <Typography fontSize={14} fontWeight={500}>{title}</Typography>
-                  <Typography fontSize={14} fontStyle="italic">Price: {currencyFormat(price)}</Typography>
-                </Stack>
-                  ))
-                }
-                </Stack>
-              </Box>
-              <Stack rowGap={1}>
-                  <Typography fontWeight={500}>
-                      Total: {amountFormatted ?? 0}
-                  </Typography>
+              justifyContent: "space-between",
+            }}
+          >
+            <Box>
+              <Typography
+                variant="h3"
+                fontSize={18}
+                sx={{
+                  marginBottom: 2,
+                }}
+              >
+                Products
+              </Typography>
+              <Stack rowGap={1} divider={<Divider />}>
+                {cart.map(({ title, price }, index) => (
+                  <Stack key={`product-item-${index}`}>
+                    <Typography fontSize={14} fontWeight={500}>
+                      {title}
+                    </Typography>
+                    <Typography fontSize={14} fontStyle="italic">
+                      Price: {currencyFormat(price)}
+                    </Typography>
+                  </Stack>
+                ))}
+              </Stack>
+            </Box>
+            <Stack rowGap={1}>
+              <Typography fontWeight={500}>
+                Total: {amountFormatted ?? 0}
+              </Typography>
               <LoadingButton
-                  loading={loading}
-                  type="submit"
-                  variant="contained"
-                  form="checkout-form"
-                  sx={{ marginX: "auto" }}
-                  disabled={disabled}
+                loading={loading}
+                type="submit"
+                variant="contained"
+                form="checkout-form"
+                sx={{ marginX: "auto" }}
+                disabled={disabled}
               >
                 Confirm payment
               </LoadingButton>
-              </Stack>
             </Stack>
           </Stack>
-        </Container>
-      </Elements>
-  )
+        </Stack>
+      </Container>
+    </Elements>
+  );
 };
 
 export default CreateAccount;
