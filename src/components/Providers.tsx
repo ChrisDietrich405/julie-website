b7a67937f9e063e078e405d888903b34df089e28
Flávio Severiano <severiano.flavio18@gmail.com>
2024-04-18 20:28:28 -0300

    refactor(structure): adjust project

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,20 +1,27 @@
 "use client";
 
-import { ThemeProvider } from "@mui/material";
-import { CartContextProvider } from "@/app/context/cartContext";
-import { UserContextProvider } from "@/app/context/userContext";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import { theme } from "@/theme";
+import {ThemeProvider} from "@mui/material";
+import {CartContextProvider} from "@/app/context/cartContext";
+import {UserContextProvider} from "@/app/context/userContext";
+import {AppRouterCacheProvider} from '@mui/material-nextjs/v13-appRouter';
+import {theme} from "@/theme";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-export const Providers = ({ children }: { children: React.ReactNode }) =>
-  (
+export const Providers = ({children}: { children: React.ReactNode }) => {
+
+  const queryClient = new QueryClient()
+
+  return (
     <AppRouterCacheProvider>
-      <ThemeProvider theme={theme}>
-        <UserContextProvider>
-          <CartContextProvider>
-            {children}
-          </CartContextProvider>
-        </UserContextProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <UserContextProvider>
+            <CartContextProvider>
+              {children}
+            </CartContextProvider>
+          </UserContextProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </AppRouterCacheProvider>
   );
+}
