7c9688c5a8424cf6ff3ba406163813e531dc1d95
Flávio Severiano <severiano.flavio18@gmail.com>
2024-06-13 19:46:13 -0300

    update project

--- src/models/availableWork.model.ts
+++ src/models/availableWork.model.ts
@@ -1,7 +1,13 @@
+import {AxiosResponse} from "axios";
+
 export interface IAvailableWork {
-    _id: string,
-    image: string,
-    price: number,
-    measurements: string,
-    title: string
+  _id: string,
+  image: string,
+  price: number,
+  measurements: string,
+  title: string
 }
+
+
+export type TAvailableWorksResponse = AxiosResponse<IAvailableWork[]>
+export type TAvailableWorkResponse = AxiosResponse<IAvailableWork>
